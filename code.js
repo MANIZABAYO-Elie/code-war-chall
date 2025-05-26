e11375e1c2fdf36bd1e319224eaaf97d5e32e99d
Elie MANIZABAYO <manizabayoelie.com>
2025-05-26 22:49:54 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,15 @@
+function sumOddCubes(arr) {
+  if (!Array.isArray(arr)) return undefined;
+
+  for (let val of arr) {
+    if (typeof val !== 'number') {
+      return undefined;
+    }
+  }
+
+  return arr
+    .map(n => Math.pow(n, 3))      // Cube each number
+    .filter(n => n % 2 !== 0)      // Keep only odd numbers
+    .reduce((sum, n) => sum + n, 0); // Sum them
+}
+console.log(sumOddCubes([1, 2, 3, 4]));     // Outputs: 28 (1^3 + 3^3 = 1 + 27)
